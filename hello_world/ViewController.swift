58b20cb953ac75cc5e7a48b3deb460b4b39f4312
doron segal <doron2402@gmail.com>
2017-02-13 19:31:52 -0800

    Initial Commit

---
+++ hello_world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello_world
+//
+//  Created by Doron Segal on 2/13/17.
+//  Copyright © 2017 Doron Segal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
